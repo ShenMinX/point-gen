fedfbe231a7331e8bd2aad2e7d919f383e39ab04
AshenRed <ashenre11@gmail.com>
2021-01-16 19:02:12 +0100

    fixed loss function by adding torch.log()

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
                 else:
                     dec_input = dec_pred.view(batch_size, 1)
 
-                p_step_loss = criterion(output, target[:,i])
+                p_step_loss = criterion(torch.log(output), target[:,i])
 
                 batch_loss = batch_loss + p_step_loss + lamada*coverage_loss
             
